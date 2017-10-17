322c063a9386c6c8bb141e574f3bbcff74af82b8
Roxanne <argonkryptonneon@gmail.com>
2017-10-17 16:55:14 -0700

    added email validation format

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,5 +5,6 @@ class Merchant < ApplicationRecord
   has_many :orderproducts, through: :orders
 
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness:true
+  validates_format_of :email, :with =>  /\A\S+@.+\.\S+\z/
 end
