5c70d27959305088a52277a05f2fca5092d235cb
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-17 16:19:37 -0700

    Categories controller actions.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,41 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(name: params[:category][:name])
+    if @category.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created #{@category.name} category"
+      redirect_to category_path
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create #{@category.name} category."
+      flash[:messages] = @category.errors.messages
+      render :new, status: :bad_request
+    end
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    if @category.destroy
+      flash[:status] = :success
+      flash[:result_text] = "Category deleted"
+      redirect_to categories_path
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "That category is unable to be deleted."
+    end
+  end
+
 end
