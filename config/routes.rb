b1fbb73c6881fa254a5392673f5bf4b3b5443b39
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-17 14:26:18 -0700

    Controllers & routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#root'
+
+
+  resources :categories
+  resources :merchants
+  resources :orders
+  resources :products
+  resources :reviews
+
 end
