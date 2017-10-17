d1e26c22f217035a8573a769ee070d6bdc5e1b34
Maria McGrew <mcgmar1@outlook.com>
2017-10-17 15:45:59 -0700

    added relationships to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  has_many :reviews
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :orders
+
 end
--- app/models/products_category.rb
+++
@@ -1,2 +0,0 @@
-class ProductsCategory < ApplicationRecord
-end
