5af5c76dee17a3f7d9d35469f0c11c70590d0523
Angela Wilson <wilsonange@gmail.com>
2017-10-18 15:13:56 -0700

    write merchant controller show tests

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -19,10 +19,21 @@ describe MerchantsController do
     it "Successfully makes a new merchant" do
       get new_merchant_path
       must_respond_with :success
-
-      # merchant = Merchant.new(username: "angela", email: "fake.email@fake.com")
-      #
-      # merchant.must_be_instance_of Merchant
     end
-  end
+  end # end new
+
+  describe "show" do
+    it "success for vaild merchant ID" do
+      get merchant_path(Merchant.first)
+      must_respond_with :success
+    end
+
+    it "gives 404 erroe page for bogus merchant ID" do
+      bogus_merchant = Merchant.last.id + 1
+
+      get merchant_path(bogus_merchant)
+      must_respond_with :not_found
+
+    end
+  end # end show
 end
