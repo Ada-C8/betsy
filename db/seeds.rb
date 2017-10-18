340133a330daf9a756823eecf5e77265d7b2fd3e
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-18 08:58:54 -0700

    kate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,20 @@ orders = Order.create([
 
 # Products: 21, 22, 23, 24
 products = Product.create([
-  { name: 'Umbra Staff', image_url: 'https://image.freepik.com/free-photo/umbrella-closed_19-121570.jpg', price: 693.39, quantity: 1, description: "This is a supremely powerful magic wand, imbued with the soul of a great wizard. It's definitely not just an umbrella.", id: 21, created_at: Time.now, updated_at: Time.now  },
-  { name: 'My Little Pony (REAL)', image_url: 'https://lilyladewig.files.wordpress.com/2009/04/glitter-pony-2.jpg', price: 123.45, quantity: 100, description: "REAL PONY REAL MAGIC!! NOT FAKE", id: 22, created_at: Time.now, updated_at: Time.now  },
-  { name: 'Witch\'s Tears', image_url: 'https://i.ebayimg.com/images/g/BlsAAOSwWnFV84vC/s-l300.jpg', price: 21.21, quantity: 1, description: "This is a supremely powerful magic wand, imbued with the soul of a great wizard. It's definitely not just an umbrella.", id: 23, created_at: Time.now, updated_at: Time.now  }
+  { name: 'Umbra Staff', image_url: 'https://image.freepik.com/free-photo/umbrella-closed_19-121570.jpg', price: 693.39, quantity: 10, description: "This is a supremely powerful magic wand, imbued with the soul of a great wizard. It's definitely not just an umbrella.", id: 21, created_at: Time.now, updated_at: Time.now, merchant_id: 22  },
+  { name: 'My Little Pony (REAL)', image_url: 'https://lilyladewig.files.wordpress.com/2009/04/glitter-pony-2.jpg', price: 123.45, quantity: 100, description: "REAL PONY REAL MAGIC!! NOT FAKE", id: 22, created_at: Time.now, updated_at: Time.now, merchant_id: 21  },
+  { name: 'Witch\'s Tears', image_url: 'https://i.ebayimg.com/images/g/BlsAAOSwWnFV84vC/s-l300.jpg', price: 21.21, quantity: 50, description: "You have to be really mean to a witch to make these.", id: 23, created_at: Time.now, updated_at: Time.now, merchant_id: 21  }
+  ])
+
+order_products = OrderProduct.create([
+  { status: 'shipped', quantity: '1', order_id: 24, product_id: 21, created_at: Time.now, updated_at: Time.now, id: 21 },
+  { status: 'shipped', quantity: '3', order_id: 24, product_id: 23, created_at: Time.now, updated_at: Time.now, id: 22 },
+  { status: 'shipped', quantity: '1', order_id: 22, product_id: 22, created_at: Time.now, updated_at: Time.now, id: 23 },
+  { status: 'shipped', quantity: '3', order_id: 21, product_id: 23, created_at: Time.now, updated_at: Time.now, id: 24 },
+  { status: 'shipped', quantity: '1', order_id: 23, product_id: 21, created_at: Time.now, updated_at: Time.now, id: 25 }
+  ])
+
+reviews = Review.create([
+  { rating: 5, description: 'very nice and magic', product_id: 22, created_at: Time.now, updated_at: Time.now, id: 21 },
+  { rating: 1, description: 'these are wizards tears, NOT witch\'s tears. seller should be ashamed', product_id: 23, created_at: Time.now, updated_at: Time.now, id: 22 }
   ])
