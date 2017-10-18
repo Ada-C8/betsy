aab87bb3e48b6c8e9d149f965540260cae6d4c40
Gale Harrington <gale.harrington19@gmail.com>
2017-10-18 15:10:32 -0700

    Validate order status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  VALID_STATS = ["pending", "complete"]
   has_and_belongs_to_many :products
-  validates :status, presence: true 
+  validates :status, presence: true, inclusion: { in: VALID_STATS }
 end
