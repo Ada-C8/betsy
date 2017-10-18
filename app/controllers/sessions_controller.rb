f21f2cfe9b19197d0a2b131b0e91750dd113903c
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-18 15:19:45 -0700

    add sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,6 +35,7 @@ class SessionsController < ApplicationController
     session[:merchant_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
+    
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index'
+
   resources :products
   resources :merchants
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/login',   to: 'sessions#login'
+  post   '/login',   to: 'sessions#create'
+  post   '/logout',  to: 'sessions#logout'
 end
