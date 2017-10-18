dbb03de404ea65844b8c48bc37e8cfcb58376ee4
Maria McGrew <mcgmar1@outlook.com>
2017-10-18 14:08:29 -0700

    must have merchant email model test passing

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,5 @@ class Merchant < ApplicationRecord
   has_many :products
 
   validates :username, presence: true
+  validates :email, presence: true
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,14 +1,20 @@
 require "test_helper"
 
 describe Merchant do
-  # let(:merchant) {Merchant.new}
-  # let(:spooky) {merchants(:spooky)} #gets books from the fixtures with this shorthand name for yml
-  # let(:ghosty) {merchants(:ghosty)}
+  let(:merchant) {Merchant.new}
+  let(:spooky) {merchants(:spooky)}
+  let(:ghosty) {merchants(:ghosty)}
 
   it "must have a merchant name to be vaild" do
-    merchant = Merchant.new(username: 'Spooky Books')
-    merchant.valid?.must_equal true
+    spooky.valid?.must_equal true
+    spooky.username = nil
+    spooky.valid?.must_equal false
+  end
 
+  it "must have a merchant email to be vaild" do
+    spooky.valid?.must_equal true
+    spooky.email = nil
+    spooky.valid?.must_equal false
   end
 
 end
