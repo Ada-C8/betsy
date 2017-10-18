18c8d0ed0f47fa77625f39d2b11193f6adcdc9f8
Amy Cash <amymcash@gmail.com>
2017-10-17 22:57:32 -0700

    Add product seed data and fill in Index Page with Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products.csv')
+puts "Loading raw product data from #{PRODUCT_FILE}"
+
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.id = row['id']
+  product.name = row['name']
+  product.description = row['description']
+  product.price = row["price"]
+  product.stock = row["stock"]
+  product.category_id = row["category_id"]
+
+  puts "Created product: #{product.inspect}"
+  successful = product.save
+  if !successful
+    product_failures << product
+  end
+end
+
+puts "Added #{Product.count} product records"
+puts "#{product_failures.length} products failed to save"
