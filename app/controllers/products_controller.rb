d09d865ca36ae7067e08083eb05cb1aaf2be284a
Tanisha <tanishahampden@gmail.com>
2017-10-18 10:03:41 -0700

    add controller action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,18 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:status] = :success
+      flash[:message] = "Successfully created product: #{@product.id}"
+      redirect_to products_path
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to create product"
+      flash.now[:details] = @product.errors..messages
+      render :new, status: :bad_request
+    end
   end
 
   def show
@@ -23,4 +35,8 @@ class ProductsController < ApplicationController
   def destroy
   end
 
+  private
+  def product_params
+    return params.require(:product).permit(:name, :description, :price, :stock ,:category_id)
+  end
 end
