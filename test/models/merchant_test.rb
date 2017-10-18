6c6d55b319fa5791de52b35b9ccdfe042b348665
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-18 12:09:35 -0700

    merchant oauth testing setup

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 describe Merchant do
   let(:merchant) { Merchant.new }
 
-  it "must be valid" do
-    value(merchant).must_be :valid?
+  # it "must be valid" do
+  #   value(merchant).must_be :valid?
+  # end
+
+  describe 'oAuth login' do
+    it 'logs in existing user' do
+      
+    end
   end
 end
