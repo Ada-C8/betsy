47e7e022d7366f289676063135842a659a5c68a7
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-18 14:12:23 -0700

    Passing product model validation tests!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -8,11 +8,37 @@ describe Product do
       products(:pointy_hat).valid?.must_equal true
 
 
+      products(:missing_name).merchant = Merchant.first
+      products(:missing_name).save
       products(:missing_name).valid?.must_equal false
 
       products(:missing_name).name = "Another pointy hat"
       products(:missing_name).save
       products(:missing_name).valid?.must_equal true
+    end
+
+    it "requires a price" do
+      test = Product.new(name: "Caldroun", quantity_avail: 1, merchant: Merchant.first)
+      test.valid?.must_equal false
+
+      test.price = 59.99
+      test.valid?.must_equal true
+    end
+
+    it "requires a quantity_avail" do
+      test = Product.new(name: "Caldroun", price: 68.50, merchant: Merchant.first)
+      test.valid?.must_equal false
+
+      test.quantity_avail = 1
+      test.valid?.must_equal true
+    end
+
+    it "requires a quantity_avail" do
+      test = Product.new(name: "Caldroun", price: 68.50, merchant: Merchant.first)
+      test.valid?.must_equal false
+
+      test.quantity_avail = 1
+      test.valid?.must_equal true
     end
 
   end
