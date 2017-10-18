65e433a6cb1838a18f67d1671243a48cf97811e4
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-18 14:03:27 -0700

    Some sort of okay tests.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :quantity_avail, presence: true
+
   belongs_to :merchant
   has_many :reviews
   has_and_belongs_to_many :categories
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
+  describe "validations" do
+    it "requires a name" do
+      products(:pointy_hat).merchant = Merchant.first
+      products(:pointy_hat).save
+      products(:pointy_hat).valid?.must_equal true
 
-  it "must be valid" do
-    value(product).must_be :valid?
+
+      products(:missing_name).valid?.must_equal false
+
+      products(:missing_name).name = "Another pointy hat"
+      products(:missing_name).save
+      products(:missing_name).valid?.must_equal true
+    end
+
   end
 end
