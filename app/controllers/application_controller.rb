66561caffae24b8e64d7f138a4e7b33dc4a9d17f
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-19 16:27:02 -0700

    cmted out "skip require log in" and tests will now run?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def require_login
+    @merchant = Merchant.find_by(id: session[:merchant_id])
+    unless @merchant
+      flash[:status] = :failure
+      flash[:message] = "You must be logged in to do that!"
+      redirect_to root_path
+    end
+  end
+
+  def save_and_flash(model)
+    result = model.save
+
+    if result
+      flash[:status] = :success
+      flash[:message] = "Successfully saved #{model.class} #{model.id}"
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to save #{model.class}"
+      flash.now[:details] = model.errors.messages
+    end
+
+    return result
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,51 +1,36 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:login]
+  # skip_before_action :require_login, only: [:login]
 
   def login
     auth_hash = request.env['omniauth.auth']
 
     if auth_hash['uid']
-      user = Merchant.find_by(provider: params[:provider], uid: auth_hash['uid'])
-      unless user
-        # user has not logged in before
-        user = Merchant.from_auth_hash(auth_hash)
-      end
+      fake_user = Merchant.find_by(provider: params[:provider], uid: auth_hash['uid'])
+      if fake_user.nil?
+        # Merchant has not logged in before
+        # Create a new record in the DB
+        fake_user = Merchant.from_auth_hash(params[:provider], auth_hash)
+        save_and_flash(fake_user)
 
-      session[:user_id] = merchant.id]
-    end
-
-    def create
-      username = params[:username]
-      if username && merchant = Merchant.find_by(username: params[:username])
-        # log em in
-        session[:merchant_id] = merchant.id
-        flash[:status] = :success
-        #congratulate them for logging in
-        flash[:result_text] = "Successfully logged in"
       else
-        # no user, try to save
-        merchant = Merchant.new(username: params[:username])
-        if merchant.save
-          # successful save
-          flash[:status] = :success
-          flash[:result_text] = "Successfully created new user!"
-          session[:merchant_id] = merchant.id
-        else
-          flash[:status] = :error
-          flash[:result_text] = "Something went wrong!"
-          render "login", status: :bad_request
-          return
-        end
+        flash[:status] = :success
+        flash[:message] = "Successfully logged in as returning user #{merchant.username}"
+
       end
-      redirect_to root_path
-      return
+      session[:merchant_id] = merchant.id
+
+    else
+      flash[:status] = :failure
+      flash[:message] = "Could not create user from OAuth data"
     end
 
-    def logout
-      session[:merchant_id] = nil
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged out"
-
-      redirect_to root_path
-    end
+    redirect_to root_path
   end
+
+  def logout
+    session[:merchant_id] = nil
+    flash[:status] = :success
+    flash[:message] = "You have been logged out"
+    redirect_to root_path
+  end
+end
