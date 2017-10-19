5c3180134eb44e9863028c2204c9cbbd3f1f2cfd
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-19 10:04:35 -0700

    pass log out tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,39 +7,40 @@ describe SessionsController do
 
   describe "login" do
     it "allows a new merchant to log in" do
-      username = "new_test_user"
+      new_test_user = "new_test_user"
 
-      Merchant.find_by(username: username).must_be_nil
+      Merchant.find_by(username: new_test_user).must_be_nil
       # should find no merchant with this username
 
-      post login_path, params: { username: username }
-      # do the actions defined in sessions#create using the information defined in username: username (in this case, username: new_test_user)
+      post login_path, params: { username: new_test_user }
+      # do the actions defined in sessions#create (as defined in routes for login_path) using the information defined in username: username (in this case, username: new_test_user)
 
       must_redirect_to root_path
     end
 
-    it "succeeds for a returning user" do
+    it "saves a new merchant's information so they can log in later" do
       skip
-      username = Merchant.first.username
-      post login_path, params: { username: username }
+    end
+
+    it "allows a returning user to log in" do
+      returning_user = Merchant.first.username
+      # this sets the username as the first existing merchant (that already exists)
+
+      post login_path, params: { username: returning_user }
+      # (same as test one)
+      # do the actions defined in sessions#create (as defined in routes for login_path) using the information defined in username: username (in this case, a returning user)
+
       must_redirect_to root_path
     end
 
-    it "renders 400 bad_request if the username is blank" do
-      skip
+    it "renders bad_request if the username entered is blank" do
       post login_path, params: { username: "" }
+
       must_respond_with :bad_request
     end
 
-    it "succeeds if a different user is already logged in" do
+    it "renders bad_request if a merchant is already logged in" do
       skip
-      username = "user_1"
-      post login_path, params: { username: username }
-      must_redirect_to root_path
-
-      username = "user_2"
-      post login_path, params: { username: username }
-      must_redirect_to root_path
     end
   end
 
@@ -54,7 +55,6 @@ describe SessionsController do
     end
 
     it "succeeds if the user is not logged in" do
-      skip
       post logout_path
       must_redirect_to root_path
     end
