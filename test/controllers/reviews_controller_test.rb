04bece041347d4968dba32238f3cd5a6e4ee22f3
Sara C <srachandler@gmail.com>
2017-10-19 09:42:41 -0700

    SC added tests to reviews controller

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,73 @@
 require "test_helper"
-
+#test for create and destroy are commented out and not working
+#might need edge cases
+#test for 404 missing failing
 describe ReviewsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "succeeds with many reviews" do
+      Review.count.must_be :>, 0
+
+      get reviews_path
+      must_respond_with :success
+    end
+    it "succeeds with no reviews" do
+      Review.destroy_all
+
+      get reviews_path
+      must_respond_with :success
+    end
+  end
+  describe "new" do
+    it "reviews" do
+      get new_review_path
+      must_respond_with :success
+    end
+  end
+#   describe "create" do
+#     it "should be able to create a review" do
+#   proc   {
+#     post reviews_path, params: { review: {rating: 3, text_review: "Smokin.", product_id: products(:pointy_hat).id}  }
+#   }.must_change 'Review.count', 1
+#
+#   must_respond_with :redirect
+#   must_redirect_to product_path
+# end
+#
+#  it "should rerender the form if it can't create the review" do
+#     proc   {
+#     post reviews_path, params: { review: {rating: 3, text_review: "Smokin.", product_id: products(:pointy_hat).id}  }
+#   }.must_change 'Review.count', 0
+#
+#       must_respond_with :success
+#     end
+#
+#   end
+  describe "show" do
+    it "can show a review" do
+      get review_path( reviews(:reviewer).id )
+
+      must_respond_with :success
+    end
+  #   it "should respond with 404 if it's not found" do
+   #
+  #    reviews(:reviewer).destroy()
+   #
+  #    get review_path( reviews(:reviewer).id )
+   #
+  #    must_respond_with :not_found
+  #  end
+  end
+  describe "destroy" do
+    #need to either fix seed data or test differently
+      # it "can delete a review" do
+      #   # test = Product.create(name: "Instant Fog", quantity_avail: 2, price: 3.99, merchant: Merchant.first)
+      #   # review = Review.new(id: 999, rating: 5, text_review: "Smokin", product_id: test.id)
+      #   # review_id = review.id
+      #   delete reviews_path( reviews(:reviewer).id )
+      #   must_redirect_to root_path
+      #
+      #
+      #   Review.find_by(id: review_id).must_be_nil
+      # end
+    end
 end
