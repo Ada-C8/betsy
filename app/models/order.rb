920159c5cc1af668290a70abd959ec8e97a7bc8f
Roxanne <argonkryptonneon@gmail.com>
2017-10-18 18:29:10 -0700

    added validations to order model and fixed fixtures to be valid

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :merchant, optional: true
   has_many :order_products
   has_many :products, through: :orderproduct
+  validates :cust_cc, :cust_cc_exp, :cust_addr, :cust_email, :status, :cust_name, presence: true
+  validates_format_of :cust_email, :with =>  /\A\w+@\w+\.\w+\z/
 end
