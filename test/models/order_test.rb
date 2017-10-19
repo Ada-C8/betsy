98a99a342452e32bc64b1bf7f812eef0bd23b696
Roxanne <argonkryptonneon@gmail.com>
2017-10-19 09:08:32 -0700

    added order fixtures and order test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 describe Order do
-  let(:order) { Order.new }
+  let(:bad_order) { Order.new(
+      merchant_id: :grace,
+      cust_name: "John",
+      cust_cc: "12345",
+      cust_cc_exp: "11/22",
+      cust_addr: "Sea World",
+      cust_email: "forkhair@mermaid.com",
+      status: "")
+  }
 
-  # it "must be valid" do
-  #   value(order).must_be :valid?
-  # end
+  describe "validations" do
+
+  end
+
+  it "must be valid" do
+    good_order.must_be :valid?
+  end
 end
