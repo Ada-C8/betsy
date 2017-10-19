92dd197ab2bfcffa0cdbad88f4b1e2689f6fd71f
Amy Cash <amymcash@gmail.com>
2017-10-19 09:52:31 -0700

    Add OrderProduct Controller and Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018214616) do
+ActiveRecord::Schema.define(version: 20171019164834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20171018214616) do
   create_table "merchants", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer "quanitity"
+    t.integer "product_id"
+    t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
