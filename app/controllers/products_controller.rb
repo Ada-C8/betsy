9b337badf63014b8f74c24fc00827ce73e5d40d6
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-19 16:04:44 -0700

    Updated routes for merchant_products path.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:merchant_id]
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      @products = merchant.products
+    else
+      @products = Product.all
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
 
   resources :categories
-  resources :merchants
+  resources :merchants do
+    get '/products', to: 'products#index'
+  end
   resources :orders
   resources :products
   resources :reviews
