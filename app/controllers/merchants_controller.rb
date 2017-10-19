0183d8950eaef0fee164800e9cff5de8e4b89c4d
Roxanne <argonkryptonneon@gmail.com>
2017-10-19 16:16:57 -0700

    testing merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,4 +2,53 @@ class MerchantsController < ApplicationController
   def login
     auth_hash = request.env['omniauth.auth']
   end
+
+  def logout
+    session[:merchant_id] = nil
+    redirect_to root_path
+  end
+  #CRUD
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  def create
+    @merchant = Merchant.new(merchant_params)
+
+    if @merchant.save
+      #success message
+      redirect_to root_path
+    else
+      #I'm not sure how we're deciding to do error messages.  Flash?
+      render :new
+    end
+  end
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    render_404 unless @merchant
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def merchant_params
+    return params.require(:merchant).permit(:username, :email, :oauth_uid, :oauth_provider)
+  end
 end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,6 +1,37 @@
 require "test_helper"
 
 describe MerchantsController do
+  describe "index" do
+    it "succeeds with many merchants" do
+      # Assumption: there are many merchants in the DB
+      # @merchants.count.must_be :>, 0
+      get merchants_path
+      must_respond_with :success
+    end
+
+    it "succeeds with no merchants" do
+      # Start with a clean slate
+      # binding.pry
+      Merchant.destroy_all
+
+      get merchants_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "succeeds for an extant merchant" do
+      get merchant_path(Merchant.first)
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus merchant" do
+      bogus_merchant_id = Merchant.last.id + 1
+      # binding.pry
+      get merchant_path(bogus_merchant_id)
+      must_respond_with :not_found
+    end
+  end
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
