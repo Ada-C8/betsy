f3905454d1600528f76eb6bcdc662160f599edf4
Amy Cash <amymcash@gmail.com>
2017-10-19 13:14:33 -0700

    Add some pseudocode

--- app/controllers/orders_products_controller.rb
+++ app/controllers/orders_products_controller.rb
@@ -2,7 +2,26 @@ class OrdersProductsController < ApplicationController
   def index
   end
 
-  
+  def new
+    # add a product (by product_id)
+    #include a quantity for that product
+    #tie it to a sepecif order number
+  end
+
+  def create
+  end
+
+  def edit
+    #remove product IDs
+  end
+
+  def update
+  end
+
+  def destroy
+    #remove all products tied to a particular order number
+    #change status to cancelled in orders_controller
+  end
 
 
 end
