6d2adba781b35d3d8b4bb34bbf405ec9eaa28d65
Tanisha <tanishahampden@gmail.com>
2017-10-20 12:31:38 -0700

    add hasmany through association

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :merchant
   belongs_to :category
   has_many :order_products
+  has_many :orders, through: :order_products
   has_many :reviews, dependent: :destroy
 
 
