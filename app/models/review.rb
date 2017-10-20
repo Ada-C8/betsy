305db40d34534b8df10977196ff9598a434501f8
Sara C <srachandler@gmail.com>
2017-10-19 20:39:07 -0700

    made nested routes for product reviews, added validation for product id in review model, made form for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :products
   validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :product_id, presence: true
 end
