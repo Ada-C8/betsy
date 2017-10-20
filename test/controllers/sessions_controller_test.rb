f99ec89768a6e8ad083d2f2ad581429f835ca2ae
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-19 22:28:19 -0700

    refactor session test 1

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,25 +2,13 @@ require "test_helper"
 
 describe SessionsController do
   describe "auth_callback" do
-    it "logs in an existing user and redirects to the root path" do
-      # make sure we're not creating a new user every time we get a login request
+    it "logs in an existing user" do
       start_count = Merchant.count
+      merchant = merchants(:fake_user)
 
-      # get a user from the fixtures
-      user = merchants(:fake_user)
-
-      # Tell OmniAuth to use this user's info when it sees an auth callback from github
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-
-      # Send a login request for that user
-      # Note that we're using the named path for the callback, as defined
-      # in the `as:` clause in `config/routes.rb`
-      get auth_callback_path(:github)
-
+      login(merchant)
       must_redirect_to root_path
-
-      # Since we can read the session, check that the user ID was set as expected
-      session[:merchant_id].must_equal user.id
+      session[:merchant_id].must_equal  merchant.id
 
       # Should *not* have created a new user
       Merchant.count.must_equal start_count
@@ -47,74 +35,74 @@ describe SessionsController do
   end
 
   # describe 'login' do
-    # it 'sign in should redirect to root_path' do
-    #   fake_user = Merchant.first
-    #
-    #   post '/auth/developer/callback', params: {
-    #     name: fake_user.username,
-    #     email: fake_user.email
-    #   }
-    #   assert_not_nil @response.cookies[Rails.application.config.session_options[:key]]
-    #   assert_equal merchant.id, session[:merchant_id]
-    #   assert_redirected_to root_path
-    # end
+  # it 'sign in should redirect to root_path' do
+  #   fake_user = Merchant.first
+  #
+  #   post '/auth/developer/callback', params: {
+  #     name: fake_user.username,
+  #     email: fake_user.email
+  #   }
+  #   assert_not_nil @response.cookies[Rails.application.config.session_options[:key]]
+  #   assert_equal merchant.id, session[:merchant_id]
+  #   assert_redirected_to root_path
+  # end
 
-    # describe "login" do
-    #   it "allows a new merchant to log in" do
-    #     username = "username"
-    #
-    #     Merchant.find_by(username: username).must_be_nil
-    #     # should find no merchant with this username
-    #
-    #     post login_path, params: { username: username }
-    #     # do the actions defined in sessions#create (as defined in routes for login_path) using the information defined in username: username (in this case, username: username)
-    #
-    #     must_redirect_to root_path
-    #   end
-    #
-    #   it "creates " do
-    #     skip
-    #   end
-    #
-    #   it "allows a returning user to log in" do
-    #     Merchant.create(username: 'finn', email: 'puppies@kittens.com')
-    #     # creates a new merchant in the db
-    #     username = Merchant.first.username
-    #     # this sets the username as the first existing merchant (that already exists)
-    #
-    #     post login_path, params: { username: username }
-    #     # (same as test one)
-    #     # do the actions defined in sessions#create (as defined in routes for login_path) using the information defined in username: username (in this case, a returning user)
-    #
-    #     must_redirect_to root_path
-    #   end
-    #
-    #   it "renders bad_request if the username entered is blank" do
-    #     post login_path, params: { username: "" }
-    #
-    #     must_respond_with :bad_request
-    #   end
-    #
-    #   it "renders bad_request if a merchant is already logged in" do
-    #     skip
-    #   end
-    # end
-    #
-    # describe "logout" do
-    #   it "succeeds if the user is logged in" do
-    #     skip
-    #     # Gotta be logged in first
-    #     post login_path, params: { username: "test user" }
-    #     must_redirect_to root_path
-    #
-    #     post logout_path
-    #     must_redirect_to root_path
-    #   end
-    #
-    #   it "succeeds if the user is not logged in" do
-    #     post logout_path
-    #     must_redirect_to root_path
-    #   end
-    # end
+  # describe "login" do
+  #   it "allows a new merchant to log in" do
+  #     username = "username"
+  #
+  #     Merchant.find_by(username: username).must_be_nil
+  #     # should find no merchant with this username
+  #
+  #     post login_path, params: { username: username }
+  #     # do the actions defined in sessions#create (as defined in routes for login_path) using the information defined in username: username (in this case, username: username)
+  #
+  #     must_redirect_to root_path
+  #   end
+  #
+  #   it "creates " do
+  #     skip
+  #   end
+  #
+  #   it "allows a returning user to log in" do
+  #     Merchant.create(username: 'finn', email: 'puppies@kittens.com')
+  #     # creates a new merchant in the db
+  #     username = Merchant.first.username
+  #     # this sets the username as the first existing merchant (that already exists)
+  #
+  #     post login_path, params: { username: username }
+  #     # (same as test one)
+  #     # do the actions defined in sessions#create (as defined in routes for login_path) using the information defined in username: username (in this case, a returning user)
+  #
+  #     must_redirect_to root_path
+  #   end
+  #
+  #   it "renders bad_request if the username entered is blank" do
+  #     post login_path, params: { username: "" }
+  #
+  #     must_respond_with :bad_request
+  #   end
+  #
+  #   it "renders bad_request if a merchant is already logged in" do
+  #     skip
+  #   end
+  # end
+  #
+  # describe "logout" do
+  #   it "succeeds if the user is logged in" do
+  #     skip
+  #     # Gotta be logged in first
+  #     post login_path, params: { username: "test user" }
+  #     must_redirect_to root_path
+  #
+  #     post logout_path
+  #     must_redirect_to root_path
+  #   end
+  #
+  #   it "succeeds if the user is not logged in" do
+  #     post logout_path
+  #     must_redirect_to root_path
+  #   end
+  # end
   # end
 end
