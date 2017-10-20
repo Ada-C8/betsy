33d58dd92db723ac18beff49ec7da141d1f86a2c
Amy Cash <amymcash@gmail.com>
2017-10-19 18:10:21 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#main'
 
-  resources :review, except: [:index, :new]
+  # resources :review, except: [:index, :new]
 
   resources :products do
     resources :reviews, only: [:index, :new]
   end
 
   resources :merchants
-  resources :categories
+  resources :categories, only: [:index, :show]
   resources :orders
   resources :order_products
 
