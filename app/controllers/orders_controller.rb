e12d9602952c91d94c32e13dd13aed5e588d372a
Tanisha <tanishahampden@gmail.com>
2017-10-20 11:56:22 -0700

    add helper method and show_cart method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,15 +4,29 @@ class OrdersController < ApplicationController
   end
 
   def new
-    #will create a new Order, with a status assigned to that id
+    # will create a new Order, with a status assigned to that id
   end
 
   def show
-    #will show order with all products listed and their quantity, as well as the status, so this info is also pulled in from the orders_products
+    # will show order with all products listed and their quantity, as well as the status, so this info is also pulled in from the orders_products
+    unless @cart
+      flash[:status] = :error
+      # make error message more detailed
+      flash[:message] = "There was an error"
+      redirect_to root_path, status: not_found
+    end
+  end
+
+  def show_cart
+    find_cart
   end
 
   def destroy
     #updates order to cancelled, removes all associated products from OP table
   end
 
+  private
+  def find_cart
+    @cart = Order.find_by(id:session[:order_id], status: "pending")
+  end
 end
