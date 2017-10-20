7df6274b35ffff4e828aebcc610ae61221a47cca
Roxanne <argonkryptonneon@gmail.com>
2017-10-20 11:46:12 -0700

    adding logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "main#index"
   get "/auth/:provider/callback", to: "merchants#login"
-
+  post '/logout', to: 'merchants#logout', as: 'logout'
   resources :merchants, except: [:index, :new] do
     resources :orders, only: [:index]
     resources :order_products, only:[:index], as: 'sold'
