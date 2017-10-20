563a651dae284d229f1230ccf5fe7408581e2303
Amy Cash <amymcash@gmail.com>
2017-10-19 18:00:51 -0700

    Add New Review Form and Create Nested Routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+
+
+def new
+  @review = Review.new
+end
+
+def create
+  @review = Review.new(review_params)
+
+  if @review.save
+    flash[:status] = :success
+    flash[:message] = "Your Review has been Created"
+    redirect_to reviews_path
+  else
+    flash.now[:status] = :failure
+    flash.now[:message] = "Review failed to save"
+    flash.now[:details] = @review.errors.messages
+    render :new, status: :bad_request
+  end
+end
+
+def show
+  @review = Review.find_by(id: params[:id])
+  unless @review
+    head :not_found
+  end
+end
+
+private
+def product_params
+  return params.require(:review).permit(:product_id, :rating, :reviewtext)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#main'
 
-  resources :products
+  resources :review, except: [:index, :new]
+
+  resources :products do
+    resources :reviews, only: [:index, :new]
+  end
+
   resources :merchants
   resources :categories
   resources :orders
   resources :order_products
-  
+
+
   get '/auth/:provider/callback', to: 'sessions#login'
   post   '/login',   to: 'sessions#create'
   post   '/logout',  to: 'sessions#logout'
