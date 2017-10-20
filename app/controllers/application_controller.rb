a83ab8f77779fe133583a4175aa6525e4362e20c
Roxanne <argonkryptonneon@gmail.com>
2017-10-20 14:54:58 -0700

    added some merchant controller tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def render_404
-    raise ActionController::RoutingError.new('Not Found')
-  end
-
 end
