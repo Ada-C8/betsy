b9d896f60b4758dbeab7baedb5bec036e89dee7a
Tanisha <tanishahampden@gmail.com>
2017-10-21 14:42:20 -0700

    add add_to_cart route

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#login', as: 'auth_callback'
   post '/logout', to: 'sessions#logout', as: 'logout'
+
+  post '/add_to_cart/:id', to: 'orders#add_to_cart'
 end
