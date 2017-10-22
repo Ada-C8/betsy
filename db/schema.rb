21c6bd71baba870606d47b6414ee9d3d20c0c5da
Maria McGrew <mcgmar1@outlook.com>
2017-10-22 11:34:46 -0700

    added city and state columns to orders db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022040645) do
+ActiveRecord::Schema.define(version: 20171022183004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20171022040645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "card_exp"
+    t.string "city"
+    t.string "state"
   end
 
   create_table "orders_products", force: :cascade do |t|
