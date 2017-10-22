8ee1e543baf7fd6419a86e02f19322e05cbe5b0a
Gale Harrington <gale.harrington19@gmail.com>
2017-10-21 22:51:39 -0700

    Add attachment photo field to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019164759) do
+ActiveRecord::Schema.define(version: 20171022054516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,10 @@ ActiveRecord::Schema.define(version: 20171019164759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
