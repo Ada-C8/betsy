87fc679e6db789a6918ac620478387f372832004
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-22 14:13:27 -0700

    Body container.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'products#home', as: "root"
+  root to: 'products#root', as: "root"
+  get '/home', to: 'products#home', as: "home"
+
 
   resources :orders, except: [:destroy]
   get '/orders/:id/confirm', to: 'orders#confirm', as: 'confirm_order'
