9fcfadb2a813c90a75be5fa74b2faaa9491e1701
Maria McGrew <mcgmar1@outlook.com>
2017-10-21 22:40:48 -0700

    added email formatting validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,14 @@ class Order < ApplicationRecord
   has_and_belongs_to_many :products
   validates :status, presence: true, inclusion: { in: VALID_STATS }
   validates :email, presence: true, if: :completed?
+  validates_format_of :email, with: /\A(\S+)@(.+)\.(\S+)\z/, message: "enter a vaild email address", allow_blank: true
   validates :address, presence: true, if: :completed?
   validates :name, presence: true, if: :completed?
   validates :card_number, presence: true, if: :completed?, numericality: true, length: {is: 16}
   validates :card_exp, presence: true, if: :completed?
   validates :card_cvv, presence: true, if: :completed?, numericality: true, length: {is: 3}
   validates :zip_code, presence: true, if: :completed?
-  validates_format_of :zip_code, with: /\A\d{5}-\d{4}|\A\d{5}\z/, message: "should be 12345 or 12345-1234", allow_blank: true
+  validates_format_of :zip_code, with: /\A\d{5}-\d{4}|\A\d{5}\z/, message: "enter a vaild zip code, 12345 or 12345-1234", allow_blank: true
 
   attribute :status, :string, default: "pending"
 
