c3f6a023d4d2224ff78e7b91fafa9c1a9c5fc93e
Roxanne <argonkryptonneon@gmail.com>
2017-10-22 13:04:52 -0700

    All tests working for Merchant Controller

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -35,63 +35,36 @@ describe MerchantsController do
       Merchant.count.must_equal start_count + 1
     end
 
-    #   it "adds order to the database and redirects when the data is valid" do
-    #     order = {
-    #       order: {
-    #         cust_name: "Mermaid",
-    #         merchant_id: 21,
-    #         cust_cc: 12345,
-    #         cust_cc_exp: "11/22",
-    #         cust_addr: "Sea World",
-    #         cust_email: "forkhair@mermaid.com",
-    #         status: "complete"
-    #       }
-    #     }
-    #     Order.new(order[:order]).must_be :valid?
-    #     start_count = Order.count
-    #
-    #     post orders_path, params: order
-    #
-    #     must_respond_with :redirect
-    #     must_redirect_to orders_path
-    #     Order.count.must_equal start_count + 1
-    #   end
-    #
-    #   it "re-renders form when the order data is invalid" do
-    #     order = {
-    #       order: {
-    #         cust_name: "Mermaid",
-    #         merchant_id: 21,
-    #         cust_cc: 12345,
-    #         cust_cc_exp: "11/22",
-    #         cust_addr: "Sea World",
-    #         cust_email: "forkhair@mermaid.com",
-    #         status: ""
-    #       }
-    #     }
-    #     Order.new(order[:order]).wont_be :valid?
-    #     start_count = Order.count
-    #
-    #     post orders_path, params: order
-    #
-    #     must_respond_with :bad_request
-    #     Order.count.must_equal start_count
-    #   end
-    # end
-    #
-    # #
-    # describe "edit" do
-    #   it "succeeds for an extant merchant ID" do
-    #     get edit_merchant_path(Merchant.first)
-    #     must_respond_with :success
-    #   end
-    #
-    #   it "renders 404 not_found for a bogus merchant ID" do
-    #     bogus_merchant_id = Merchant.last.id + 1
-    #     # binding.pry
-    #     get edit_merchant_path(bogus_merchant_id)
-    #     must_respond_with :not_found
-    #   end
+
+    it "re-renders form when the merchant data is invalid" do
+      merchant = {
+        merchant: {
+          oauth_provider: "github",
+          oauth_uid: "",
+          email: "somebodyelse@somesite.com",
+          username: "Somebody_else"
+        }
+      }
+      Merchant.new(merchant[:merchant]).wont_be :valid?
+      start_count = Merchant.count
+
+      post merchants_path, params: merchant
+
+      must_respond_with :bad_request
+      Merchant.count.must_equal start_count
+    end
+  end
+
+  describe "edit" do
+    it "succeeds for an extant merchant ID" do
+      get edit_merchant_path(Merchant.first)
+      must_respond_with :success
+    end
+    it "renders 404 not_found for a bogus merchant ID" do
+      bogus_merchant_id = Merchant.last.id + 1
+      get edit_merchant_path(bogus_merchant_id)
+      must_respond_with :not_found
+    end
   end
 
   describe "update" do
