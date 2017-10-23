e32dce94d0b42e25507ae2bd739dff5eae6824f1
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-23 00:25:37 -0700

    add comments explaining all the add-to-cart logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,32 +2,24 @@ class OrdersController < ApplicationController
   skip_before_action :require_login
 
   def index
+    # placeholder for error message (empty cart)
   end
 
   def add_to_cart
-    order = Order.find_or_create_cart(session[:order_id])
-    session[:order_id] = order.id
-    product = Product.find_by(id: params[:id])
-    order.products << product # magic ruby method
+    order = Order.find_or_create_cart(session[:order_id]) # find or create a cart (in order.rb)
+    session[:order_id] = order.id # the session's order_id will be reset to the current order.id (was either nil or existed already--"unless")
+    product = Product.find_by(id: params[:id]) # find the product from the URL param
+    order.products << product # (the has_many declaration creates some cool ruby magic methods like this)
 
     redirect_to product_path(product)
   end
 
   def show
-    # will show order with all products listed and their quantity,
-    # as well as the status,
-    # so this info is also pulled in from the orders_products
-
-    @cart = Order.find_or_create_cart(session[:order_id])
-  end
-
-  def show_cart
-    # find_cart
+    @cart = Order.find_or_create_cart(session[:order_id]) # cart instance
   end
 
   def destroy
     #updates order to cancelled, removes all associated products from OP table
   end
-
   # moved private methods to model
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,18 +2,21 @@ class Order < ApplicationRecord
   has_many :order_products
   has_many :products, through: :order_products
 
+# class methods instead of instance methods (like in ctrl)
   def self.start_new_order
-    # create new instance of order and set status to "pending"
     order = Order.new
     order.status = "pending"
     order.save
     return order
   end
 
+  # order_id being passed in is session[:order_id] from order ctrl, cannot use sessions in model methods
+  ## this method can be used with any other parameter passed in, this is just the current use!
   def self.find_or_create_cart(order_id)
     order = Order.find_by(id: order_id, status: "pending") # rails won't blow up if the session id is nil, turns out
+    # checking if the order has BOTH the same order_id and status "pending"
     unless order
-      order = start_new_order
+      order = start_new_order # if no matching orders, start a new order 
     end
     return order
   end
