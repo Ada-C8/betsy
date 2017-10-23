1290e27fdb7182aa00fa5a6baffaabb70caa19f3
Amy Cash <amymcash@gmail.com>
2017-10-23 14:41:22 -0700

    Add Category Validations and Associated Tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,38 +5,41 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new(category_params)
+  end
 
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:status] = :success
+      flash[:message] = "Successfully created category: #{@category.id}"
+      redirect_to categories_path
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to create category"
+      flash.now[:details] = @category.errors.messages
+      render :new, status: :bad_request
+    end
+
+    # def edit
+    # end
+    #
+    # def update
+    # end
+    #
+    # def delete
+    # end
 
   def show
     @category = Category.find_by(id: params[:id])
   end
+  end
 
-  # def edit
-  # end
-  #
-  # def update
-  # end
-  #
-  # def delete
-  # end
-  # def new
-  #   @category = Category.new
-  # end
 
-  # def create
-  #   @category = Category.new(category_params)
-  #
-  #   if @category.save
-  #     flash[:status] = :success
-  #     flash[:message] = "Successfully created category: #{@category.id}"
-  #     redirect_to categories_path
-  #   else
-  #     flash.now[:status] = :failure
-  #     flash.now[:message] = "Failed to create category"
-  #     flash.now[:details] = @category.errors.messages
-  #     render :new, status: :bad_request
-  #   end
-  # end
+
   private
   def category_params
     return params.require(:category).permit(:category_name)
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  validates :category_name, uniqueness: true, length: { maximum: 30, too_long: "%{30} characters is the maximum allowed" }
+  
   has_many :products
 end
