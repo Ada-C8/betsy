cbc95572705adabb8ee8eea29ff473b9fad0333f
Amy Cash <amymcash@gmail.com>
2017-10-23 15:50:07 -0700

    Add Edit and Delete Functions for Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  skip_before_action :require_login, only: [:index, :show, :new, :create]
+  skip_before_action :require_login, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -25,18 +25,33 @@ class CategoriesController < ApplicationController
     end
   end
 
-    # def edit
-    # end
-    #
-    # def update
-    # end
-    #
-    # def delete
-    # end
-
-    def show
-      @category = Category.find_by(id: params[:id])
+  def edit
+    @category = Category.find_by(id: params[:id])
+    unless @category
+      head :not_found
     end
+  end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+    @category.update_attributes(category_params)
+
+    if @category.save
+      redirect_to product_path(@category)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+  end
 
   private
   def category_params
