30c7921e61783944fd89ec79ab4f1a9718cdbfbd
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-24 11:21:24 -0700

    edit billing validation

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,7 +1,7 @@
 class Billing < ApplicationRecord
-  validates :street, :city, :state, :email, :cvv, presence: true
-  validates :ship_zip, :bill_zip, presence: true, length: 5
-  validates :credit_card, presence: true, length: 16
+  validates :name, :street, :city, :state, :email, :cvv, presence: true
+  validates :ship_zip, :bill_zip, presence: true, length: { is: 5}
+  validates :credit_card, presence: true, length: { is: 16 }
   # all these things are required
   # apt is optional
 
