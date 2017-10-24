dfc22cf9e9133949637673e452f0760f2c72e6e9
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-24 10:57:32 -0700

    add comma

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,5 +1,5 @@
 class Billing < ApplicationRecord
-  validates :street, :city, :state, :email :cvv, presence: true
+  validates :street, :city, :state, :email, :cvv, presence: true
   validates :ship_zip, :bill_zip, presence: true, length: 5
   validates :credit_card, presence: true, length: 16
   # all these things are required
