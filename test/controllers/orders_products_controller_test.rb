24aa3950de35925b926d2968953a4293adc428f7
Tanisha <tanishahampden@gmail.com>
2017-10-24 12:03:52 -0700

    update associations test

--- test/controllers/orders_products_controller_test.rb
+++ test/controllers/orders_products_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 describe OrdersProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "associations" do
+    it "responds to order" do
+      order_product = OrderProduct.new
+      order_product.must_respond_to :order
+    end
+    it "responds to product" do
+      order_product = OrderProduct.new
+      order_product.must_respond_to :product
+    end
+  end
 end
