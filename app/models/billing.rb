d69b9c96dbe4ea21159e738daa57ea91a25dd1c0
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-24 10:33:03 -0700

    zip code to 5 char

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,6 +1,6 @@
 class Billing < ApplicationRecord
   validates :street, :city, :state, :email, :exp, :cvv, :bill_zip, presence: true
-  validates :ship_zip, :bill_zip, presence: true, length: 4
+  validates :ship_zip, :bill_zip, presence: true, length: 5
   validates :credit_card, presence: true, length: 16
   validates :cvv, presence: true, length: 3
   # all these things are required
