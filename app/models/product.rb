35412b495df4cb0ee6e39826adfdcef97abcbf96
Angela Wilson <wilsonange@gmail.com>
2017-10-23 19:51:56 -0700

    made subtract inventory methed to order model. needs testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,11 @@ class Product < ApplicationRecord
 
   validates :price, presence: true, numericality: { greater_than: 0 }
 
+  validates :stock, presence: true, numericality: {
+    greater_than_or_equal_to: 0,
+    only_integer: true
+    }
+
   validates :merchant_id, presence: true
 
   validates :category_id, presence: true
