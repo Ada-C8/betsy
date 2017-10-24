bc71a92cc37fc46e6e2ff47610404d7b1559346b
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-24 09:29:02 -0700

    add int length validations to billing model

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,5 +1,8 @@
 class Billing < ApplicationRecord
-  validates :street, :city, :state, :ship_zip, :email, :credit_card, :exp, :cvv, :bill_zip, presence: true
+  validates :street, :city, :state, :email, :exp, :cvv, :bill_zip, presence: true
+  validates :ship_zip, :bill_zip, presence: true, length: 4
+  validates :credit_card, presence: true, length: 16
+  validates :cvv, presence: true, length: 3
   # all these things are required
   # apt is optional
 
