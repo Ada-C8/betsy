e86d4848278eb244f8b22f84b38cc9b74cb1453e
kee nam <nam.keeyeon@gmail.com>
2017-10-25 11:17:14 -0700

    Added order_status method to orders model

--- app/models/order.rb
+++ app/models/order.rb
@@ -35,14 +35,13 @@ class Order < ApplicationRecord
   end
 
   def order_status
-    shipped_count = 0
-    order_products.each do |order_product|
-      if order_product.status == "shipped"
-        shipped_count += 1
-      end
+    # shipped_count = 0
+    result = order_products.all? do |order_product|
+      order_product.status == "shipped"
     end
-    if shipped_count == order_products.length
-      self.status == "complete"
-    end
+
+    # if shipped_count == order_products.length
+      self.status = "complete" if result
+    # end
   end
 end
