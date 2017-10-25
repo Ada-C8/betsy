a8791d0df9f39fcab04a37ca31e8dd57abac739b
Amy Cash <amymcash@gmail.com>
2017-10-25 10:42:35 -0700

    Fix Category and Review Model Tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5  }
 
-  validates :reviewtext, presence: true
+  validates :reviewtext, length: { maximum: 500, too_long: "Review must be under 500 characters." }
 end
