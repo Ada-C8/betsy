a71f5fd908982c9dc535d08215f4cedfb4c0b412
Amy Cash <amymcash@gmail.com>
2017-10-25 14:32:51 -0700

    More Category COntroller Tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -51,6 +51,9 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by(id: params[:id])
+    unless @category
+      head :not_found
+    end
   end
 
   private
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'pry'
 
 describe CategoriesController do
 
@@ -6,7 +7,7 @@ describe CategoriesController do
     it "gets a list of categories" do
       get categories_path
       must_respond_with :success
-    end #index
+    end #gets list of categories
 
     it "returns a success status when no categories" do
       Category.destroy_all
@@ -21,61 +22,77 @@ describe CategoriesController do
       must_respond_with :success
     end #return success
   end #new tests
-end #all tests
-## NEED TO ADD FIXTURES !!
+
+  ## NEED TO ADD FIXTURES !!
 
   describe "create" do
     it "creates a valid category when data is valid" do
-      # test failing because i need to add test data for merchant and category
 
-      # category_data = {
-      #   category: {
-      #     category_name: "abcdefg"
-      #   }
-      # }
+      category_data = {
+        category: {
+          category_name: "abcdefg"
+        }
+      }
+
       start_category_count = Category.count
-      # c = Category.new(category_data[:category])
-      c = Category.new
-      c.category_name = "abcdefg"
-      c.must_be :valid?
-      c.save
+      Category.new(category_data[:category]).must_be :valid?
 
-      # # post categories_url, params: { category: { category_name: "ABCDE"} }
-      # # must_respond_with 302
-      # # must_redirect_to categories_url
-      #
+      # Act
+      post categories_path, params: category_data
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to categories_path
+
       Category.count.must_equal start_category_count + 1
+
     end #valid data test
 
 
     it "will not create a new cateogry when data is invalid" do
-      start_category_count = Category.count
-      c = Category.new
-      c.category_name = ""
-      c.wont_be :valid?
-      c.save
+        invalid_category_data = {
+          category: {
+            category_name: ""
+          }
+        }
+        start_category_count = Category.count
 
-      #
-      # post products_path, params: invalid_product_data
-      # must_respond_with :bad_request
+        Category.new(invalid_category_data[:category]).wont_be :valid?
 
-      Category.count.must_equal start_category_count
-    end
+        post categories_path, params: invalid_category_data
+        must_respond_with :bad_request
 
-  end #create tests
-  # describe "show" do
-  #   it "return success when given a valid work id" do
-  #     product_id = Product.first.id
-  #     get product_path(product_id)
-  #     must_respond_with :success
-  #   end
-  #   it "returns not found when given an invalid product id" do
-  #     product_id = Product.first.id + 1
-  #     get product_path(product_id)
-  #     must_respond_with :not_found
-  #   end
-  # end
-  #
+        Category.count.must_equal start_category_count
+        # start_category_count = Category.count
+        # c = Category.new
+        # c.category_name = ""
+        # c.wont_be :valid?
+        # c.save
+        #
+        # Category.count.must_equal start_category_count
+      end #invalid data test
+    end #create tests
+
+
+    describe "show" do
+      it "should get show" do
+        get category_path(categories(:cat_one).id)
+        must_respond_with :success
+      end #get show
+
+      it "return success when given a valid category id" do
+        category_id = Category.first.id
+        get category_path(category_id)
+        must_respond_with :success
+      end #return success
+
+      it "returns not found when given an invalid product id" do
+        category_id = Category.last.id + 1
+        get category_path(3)
+        must_respond_with :not_found
+      end #return not found
+    end #return show
+  end #all tests
   # describe "edit" do
   #   it "returns success when given a valid product id" do
   #     product_id = Product.first.id
@@ -122,4 +139,3 @@ end #all tests
   #     must_respond_with :redirect
   #   end
   # end
-# end
