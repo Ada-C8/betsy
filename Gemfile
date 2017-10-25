50d9ec1d1fa7210d102155ab0c8300691ab07e2e
Sara C <srachandler@gmail.com>
2017-10-25 11:02:05 -0700

    changed application controller flash message to result text and added status

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'dotenv-rails'
+  gem 'simplecov'
 end
 
 group :development do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,8 @@ Minitest::Reporters.use!(
  Minitest.backtrace_filter
 )
 
-
+require 'simplecov'
+SimpleCov.start
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
