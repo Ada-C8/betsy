29c2aa2a81c4134f9ee875efb6d7609b578c3f95
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-25 19:45:16 -0700

    tests for add_to_cart

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,22 +1,27 @@
 class MainController < ApplicationController
 
+  before_action only: [:add_to_cart] do
+    find_object_by_params(Product)
+  end
+
   def index
   end
 
   def add_to_cart
-    if session[:cart].nil?
-      session[:cart] = []
-    end
-    find_object_by_params(Product)
     order_product = OrderProduct.create(quantity: params["quantity"], product_id: @product.id, status: "pending")
-    session[:cart] << order_product.id
-    flash[:status] = :success
-    flash[:message] = "#{@product.name} added to cart."
-    redirect_to products_path
+    if order_product.valid?
+      session[:cart] << order_product.id
+      flash[:status] = :success
+      flash[:message] = "#{@product.name} added to cart."
+      redirect_to products_path
+    else
+      flash[:status] = :failure
+      flash[:message] = "#{@product.name} could not be added to cart."
+      redirect_to products_path
+    end
   end
 
   def shopping_cart
-    session[:cart] ||= []
     @products = OrderProduct.find_in_cart(session[:cart])
   end
 end
--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -9,6 +9,12 @@ describe MainController do
     must_respond_with :success
   end
 
+  it 'can successfully access the shopping cart page' do
+    get shopping_cart_path
+
+    must_respond_with :success
+  end
+
   describe '#add_to_cart' do
     it 'adds items to cart' do
       params = {
@@ -18,10 +24,35 @@ describe MainController do
 
       post add_to_cart_path(wand.id), params: params
 
-      binding.pry
-
-      must_respond_with :success
+      must_respond_with :found
       flash[:status].must_equal :success
+      session[:cart].length.must_equal 1
+    end
+
+    it 'returns not_found for DNE product' do
+      bad_id = Product.last.id + 1
+      params = {
+        "quantity" => "4",
+        "id" => bad_id
+      }
+
+      post add_to_cart_path(bad_id), params: params
+
+      must_respond_with :not_found
+      session[:cart].length.must_equal 0
+    end
+
+    it 'wont work if no quantity is passed' do
+      params = {
+        "id" => wand.id
+      }
+
+      post add_to_cart_path(wand.id), params: params
+
+      must_respond_with :found
+      flash[:status].must_equal :failure
+
+      session[:cart].length.must_equal 0
     end
   end
 end
