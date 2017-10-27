a9a76c714335781ad96f69389c38d7419c79563c
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-27 12:52:05 -0700

    no change

--- app/models/order.rb
+++ app/models/order.rb
@@ -42,6 +42,7 @@ class Order < ApplicationRecord
     items = {} # keys are the product, values are the quantity
 
     self.products.each do |product|
+
       if items.include?(product)
         items[product] += 1 # if the current product is in the cart, add 1 to its value
       else
